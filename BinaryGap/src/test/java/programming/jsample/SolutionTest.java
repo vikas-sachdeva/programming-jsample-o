c00bc420cb904891baf5fe232723cad6a50bd9eb
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2018-01-18 16:25:16 +0900

    test method name updated

--- BinaryGap/src/test/java/programming/jsample/SolutionTest.java
+++ BinaryGap/src/test/java/programming/jsample/SolutionTest.java
@@ -8,37 +8,37 @@ public class SolutionTest {
 	private Solution solution = new Solution();
 
 	@Test
-	public void solutionTest1() {
+	public void test1Solution() {
 		int binaryGap = solution.solution(9);
 		Assert.assertEquals(2, binaryGap);
 	}
 
 	@Test
-	public void solutionTest2() {
+	public void test2Solution() {
 		int binaryGap = solution.solution(529);
 		Assert.assertEquals(4, binaryGap);
 	}
 
 	@Test
-	public void solutionTest3() {
+	public void test3Solution() {
 		int binaryGap = solution.solution(20);
 		Assert.assertEquals(1, binaryGap);
 	}
 
 	@Test
-	public void solutionTest4() {
+	public void test4Solution() {
 		int binaryGap = solution.solution(15);
 		Assert.assertEquals(0, binaryGap);
 	}
 
 	@Test
-	public void solutionTest5() {
+	public void test5Solution() {
 		int binaryGap = solution.solution(1041);
 		Assert.assertEquals(5, binaryGap);
 	}
-	
+
 	@Test
-	public void solutionTest6() {
+	public void test6Solution() {
 		int binaryGap = solution.solution(1);
 		Assert.assertEquals(0, binaryGap);
 	}
